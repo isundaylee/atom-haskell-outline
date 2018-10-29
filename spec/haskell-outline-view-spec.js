699c7151bea79f62f025e203e3618dedb8fcfd5a
Jiahao Li <reg@ljh.me>
2018-10-29 17:45:50 -0400

    Basic outline test

--- spec/haskell-outline-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import HaskellOutlineView from '../lib/haskell-outline-view';
-
-describe('HaskellOutlineView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
