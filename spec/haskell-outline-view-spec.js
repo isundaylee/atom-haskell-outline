ab2d2152b27948bcd6cb568affd4b72ca7f59f42
Jiahao Li <reg@ljh.me>
2018-10-29 17:25:13 -0400

    Initial commit

---
+++ spec/haskell-outline-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HaskellOutlineView from '../lib/haskell-outline-view';
+
+describe('HaskellOutlineView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
